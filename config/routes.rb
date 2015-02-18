d0fc1abdbd0953b30421906f2d6282d50da5c0b6
Alen Lukac <alen909@Alens-MacBook-Pro.local>
2015-02-18 02:43:52 +0100

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'frustrations#index'
-  get 'static_pages/about'
+  get 'about' => 'static_pages#about'
   resources :users, only: [ :new, :create, :show ]
   resources :sessions, only: [:new, :create]
   get 'auth/:provider/callback', to: 'sessions#create'
